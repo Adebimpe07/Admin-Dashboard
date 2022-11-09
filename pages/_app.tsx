70c7938de755a17d880e0248f432a38ee9c8dd52
Adebimpe07 <adebimmy@gmail.com>
2022-11-09 17:35:02 +0100

    Merged updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,19 @@ import type { AppProps } from "next/app";
 import { ModalsProvider } from "@mantine/modals";
 import { MantineProvider } from "@mantine/core";
 import HeaderMain from "../src/components/main/header";
+import Aside from "../src/components/aside/aside";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider>
-      <ModalsProvider>
-        <HeaderMain />
-        <Component {...pageProps} />
-      </ModalsProvider>
-    </MantineProvider>
+    <div className="flex">
+      <MantineProvider>
+        <ModalsProvider>
+          {/* <HeaderMain /> */}
+          <Aside />
+          <Component {...pageProps} />
+        </ModalsProvider>
+      </MantineProvider>
+    </div>
   );
 }
 export default MyApp;
--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import Body from "../../src/components/main/body/body";
 import Header from "../../src/components/main/body/header";
+import HeaderJob from "../../src/components/main/body/headerJob";
 import All from "../../src/components/main/body/job";
-
 
 const index = () => {
   const [selected, setSelected] = useState(0);
   return (
-    <div className="bg-[#E5E5E5] h-screen">
-      <Header selected={selected} setSelected={setSelected}/>
-      {selected ===0? <All />: <Body/>}
-      
+    <div className="bg-[#E5E5E5] flex-1 h-screen">
+      <HeaderJob />
+      <Header selected={selected} setSelected={setSelected} />
+      {selected === 0 ? <All /> : <Body />}
     </div>
   );
 };
