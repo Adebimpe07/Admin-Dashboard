94bb641de35479ed920f4ce387cbaec3797fc79e
adeTunes <babatundeadebowale1998@gmail.com>
2022-12-14 14:25:39 +0100

    changes to login

--- src/components/main/admin/loginPage.tsx
+++ src/components/main/admin/loginPage.tsx
@@ -11,81 +11,80 @@ import { useRouter } from "next/router";
 import Loading from "../../loading";
 import CryptoJS from "crypto-js";
 const loginPage = () => {
-  const { setToken, setAdmin } = useContext(FormContext);
-  const [visible, { toggle }] = useDisclosure(false);
-  const [loading, setLoading] = useState(false);
-  var key = CryptoJS.enc.Utf8.parse("bQeThWmZq4t7w9z$C&F)J@NcRfUjXn2r");
-  var iv = CryptoJS.enc.Utf8.parse("s6v9y$B&E)H@McQf");
-  const router = useRouter();
-  const form = useForm({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-  });
-  const handleSubmit = (e) => {
-    setLoading(true);
-    e.preventDefault();
-    var config = {
-      method: "post",
-      url: `${process.env.NEXT_PUBLIC_BASE_URL_1}/api/v1/account/admin/login`,
-      data: form.values,
+    const { setToken, setAdmin } = useContext(FormContext);
+    const [visible, { toggle }] = useDisclosure(false);
+    const [loading, setLoading] = useState(false);
+    var key = CryptoJS.enc.Utf8.parse("bQeThWmZq4t7w9z$C&F)J@NcRfUjXn2r");
+    var iv = CryptoJS.enc.Utf8.parse("s6v9y$B&E)H@McQf");
+    const router = useRouter();
+    const form = useForm({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+    });
+    const handleSubmit = (e) => {
+        setLoading(true);
+        e.preventDefault();
+        var config = {
+            method: "post",
+            url: `${process.env.NEXT_PUBLIC_BASE_URL_1}/api/v1/account/admin/login`,
+            data: form.values,
+        };
+
+        // refresh token and access token
+
+        axios(config)
+            .then(function (response) {
+                setToken(response.data.data);
+                // console.log(response.data.data);
+                setAdmin(
+                    jwtDecode(
+                        CryptoJS.AES.decrypt(response.data.data.access, key, {
+                            iv: iv,
+                        }).toString(CryptoJS.enc.Utf8)
+                    )
+                );
+                setLoading(false);
+                router.push("/");
+            })
+            .catch(function (error) {
+                console.log(error);
+                setLoading(false);
+            });
     };
 
-    // refresh token and access token
-
-    axios(config)
-      .then(function (response) {
-        setToken(response.data.data);
-        console.log(response.data.data);
-        console.log(
-          jwtDecode(
-            CryptoJS.AES.decrypt(response.data.data.access, key, {
-              iv: iv,
-            }).toString(CryptoJS.enc.Utf8)
-          )
-        );
-        setLoading(false);
-        router.push("/");
-      })
-      .catch(function (error) {
-        console.log(error);
-        setLoading(false);
-      });
-  };
-
-  return (
-    <form className="bg-[#E5E5E5] flex-1 flex flex-col items-center justify-center pb-8 h-screen">
-      <img width="124px" src={Logo.src} alt="" />
-      <div className="bg-[#fff] flex flex-col rounded-2xl p-7 leading-5 w-[30rem] mt-4 shadow-xl shadow-[0px 51.8664px 76.6208px rgba(193, 194, 198, 0.19)]">
-        <p>Sign in to ATS Admin</p>
-        <div>
-          <TextInput
-            placeholder="email.com"
-            label="Email"
-            withAsterisk
-            {...form.getInputProps("email")}
-          />
-          <PasswordInput
-            withAsterisk
-            label="Password"
-            visible={visible}
-            onVisibilityChange={toggle}
-            {...form.getInputProps("password")}
-          />
-        </div>
-        <Checkbox label="Remember me" className="mt-4" />
-        <Button
-          onClick={(e) => handleSubmit(e)}
-          className="bg-greenButton hover:bg-greenButton my-4"
-        >
-          Sign in
-        </Button>
-        <EnterEmail />
-      </div>
-      <Loading loading={loading} />
-    </form>
-  );
+    return (
+        <form className="bg-[#E5E5E5] flex-1 flex flex-col items-center justify-center pb-8 h-screen">
+            <img width="124px" src={Logo.src} alt="" />
+            <div className="bg-[#fff] flex flex-col rounded-2xl p-7 leading-5 w-[30rem] mt-4 shadow-xl shadow-[0px 51.8664px 76.6208px rgba(193, 194, 198, 0.19)]">
+                <p>Sign in to ATS Admin</p>
+                <div>
+                    <TextInput
+                        placeholder="email.com"
+                        label="Email"
+                        withAsterisk
+                        {...form.getInputProps("email")}
+                    />
+                    <PasswordInput
+                        withAsterisk
+                        label="Password"
+                        visible={visible}
+                        onVisibilityChange={toggle}
+                        {...form.getInputProps("password")}
+                    />
+                </div>
+                <Checkbox label="Remember me" className="mt-4" />
+                <Button
+                    onClick={(e) => handleSubmit(e)}
+                    className="bg-greenButton hover:bg-greenButton my-4">
+                    Sign in
+                </Button>
+                <EnterEmail />
+            </div>
+            <Loading loading={loading} />
+        </form>
+    );
 };
 
 export default loginPage;
