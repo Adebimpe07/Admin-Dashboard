cb985646f41bee908c6f051d328f9e4f11ea7939
oladejo toyyib <olatoyconsult@gmail.com>
2022-12-14 09:44:02 +0100

    changes on gallery

---
+++ pages/content/gallery/index.tsx
@@ -0,0 +1,23 @@
+import dynamic from "next/dynamic";
+import React from "react";
+import Album from "../../../src/components/main/body/contentPage/album";
+import ContentHeader from "../../../src/components/main/body/contentPage/contentHeader";
+import GallerySubheader from "../../../src/components/main/body/contentPage/gallerySubheader";
+import { albumData } from "../../../src/layout/albumData";
+
+const ContentGallery = dynamic(
+    () => import("../../../src/components/main/body/contentPage/contentGallery"),
+    { ssr: false }
+);
+
+const gallery = () => {
+    return (
+        <div className="flex-1 bg-mainBg flex flex-col overflow-auto  h-full">
+            <ContentHeader />
+            <GallerySubheader />
+            {albumData.length > 0 ? <Album /> : <ContentGallery />}
+        </div>
+    );
+};
+
+export default gallery;
