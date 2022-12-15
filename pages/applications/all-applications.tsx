61a8e56299ec9fa9ae5e43cd5bd4c1c68758d53b
Adebimpe Adeoye <adebimmy@gmail.com>
2022-12-15 09:09:56 +0100

    new commit changes

--- pages/applications/all-applications.tsx
+++ pages/applications/all-applications.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import HeaderApp from "../../src/components/main/body/applicationPage/headerApp";
 import SubAppHeader from "../../src/components/main/body/applicationPage/subAppHeader";
 import All from "../../src/components/main/body/jobPage/job";
 import Body from "../../src/components/main/body/jobPage/body";
@@ -7,11 +6,12 @@ import TableHead from "../../src/components/main/body/applicationPage/tableHead"
 import dynamic from "next/dynamic";
 import FormContext from "../../src/context/store";
 import ApplicationBody from "../../src/components/main/body/applicationPage/ApplicationBody";
+import Header from "../../src/components/header";
 
 const Index = () => {
   return (
     <div className="flex-1 bg-mainBg flex flex-col overflow-auto pb-4 h-full">
-      <HeaderApp />
+      <Header name="Application Management" />
       <ApplicationBody />
     </div>
   );
