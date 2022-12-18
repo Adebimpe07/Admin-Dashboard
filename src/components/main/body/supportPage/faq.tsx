f308d45af78e00e3b351e4d78eef8e271d11cf3f
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2022-12-18 16:53:54 +0100

    new update

--- src/components/main/body/supportPage/faq.tsx
+++ src/components/main/body/supportPage/faq.tsx
@@ -42,6 +42,7 @@ const Faqs = () => {
     });
     const [questions, setQuestions] = useState([]);
     const access = JSON.parse(sessionStorage.getItem("token")).access;
+    console.log(access)
 
     const fetchFaqs = () => {
         var config = {
@@ -75,7 +76,13 @@ const Faqs = () => {
         fetchFaqs();
     }, []);
 
-    const saveFaqs = () => {
+    const loopAndSend = () => {
+        form.values.faqs.map(item => {
+            saveFaqs({question: encrypt(item.question), answer: encrypt(item.answer)})
+        })
+    }
+
+    const saveFaqs = (data: {question: string, answer: string}) => {
         var config = {
             method: "post",
             url: `${process.env.NEXT_PUBLIC_BASE_URL_1}/api/v1/support/FAQ-list-create/`,
@@ -85,7 +92,7 @@ const Faqs = () => {
                 "request-ts": `${process.env.NEXT_PUBLIC_REQUEST_TS_1}`,
                 Authorization: `Bearer ${access}`
             },
-            // data: {}
+            data: data
         };
 
         axios(config)
@@ -171,7 +178,7 @@ const Faqs = () => {
                     </div>
                 </article>
                 <button
-                    onClick={saveFaqs}
+                    onClick={loopAndSend}
                     className="w-full align-middle bg-green-400 border rounded-xl text-white py-3">
                     save
                 </button>
